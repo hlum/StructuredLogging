3d65aad4c9654bc2d0678bf2f35dfabee9a98f82
アウン <hlumaungphyo@gmail.com>
2025-01-26 20:23:17 +0900

    project created

---
+++ StructuredLoggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuredLoggingApp.swift
+//  StructuredLogging
+//
+//  Created by Hlwan Aung Phyo on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct StructuredLoggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
